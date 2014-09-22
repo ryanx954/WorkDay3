76e46414f772f177bf0700155208fbae3253c841
idsc3101 <idsc3101@gmail.com>
2014-09-22 17:14:25 -0500

    WorkDay3

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -3,28 +3,34 @@
 Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
-        Dim num1, num2, largerNum As Double
+        Dim num1, num2 As Double
 
-        ' Modify the program such that it does not crash 
-        ' if the user types incorrect input
+            Dim st1 As String = txtFirstNum.Text
+            Dim st2 As String = txtSecondNum.Text
 
-        Dim st1 As String = txtFirstNum.Text
-        Dim st2 As String = txtSecondNum.Text
+        If IsNumeric(st1) And IsNumeric(st2) Then
 
-        num1 = CDbl(st1)
-        num2 = CDbl(st2)
+            num1 = CDbl(st1)
+            num2 = CDbl(st2)
 
-        Dim result As String
+            Dim result As String
 
-        If num1 > num2 Then
-            result = "The larger number is " & num1
-        ElseIf num2 > num1 Then
-            result = "The larger number is " & num2
-        Else
-            result = "They are equal"
+            If num1 > num2 Then
+                result = "The larger number is " & num1
+            ElseIf num2 > num1 Then
+                result = "The larger number is " & num2
+            Else
+                result = "They are equal"
+            End If
+
+            txtResult.Text = result
+            Return
+
+        ElseIf Not IsNumeric(st1) Or Not IsNumeric(st2) Then
+            MessageBox.Show("Error!")
+
+
         End If
 
-        txtResult.Text = result
     End Sub
-
 End Class
